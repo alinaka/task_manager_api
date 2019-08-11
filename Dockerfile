1577000e1684295835f389011d1d83ecf1c0f14a
Alina Gotovtseva <alinakago@gmail.com>
2019-08-11 23:25:24 +0300

    adds job preserving

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ RUN mkdir /code
 WORKDIR /code
 COPY . /code/
 RUN pip install -r requirements.txt
+RUN mkdir /var/data/
 COPY ./docker-entrypoint.sh /docker-entrypoint.sh
 RUN chmod +x /docker-entrypoint.sh
 ENTRYPOINT ["/docker-entrypoint.sh"]
--- bot/bot.py
+++ bot/bot.py
@@ -1,6 +1,8 @@
 import logging
-from datetime import datetime
-from time import sleep
+import pickle
+from datetime import datetime, timedelta
+from time import sleep, time
+from threading import Event
 
 from django.conf import settings
 from telegram import (InlineKeyboardButton, InlineKeyboardMarkup,
@@ -277,8 +279,81 @@ def no_deadline(bot, update):
 ) = range(11)
 
 
+JOBS_PICKLE = 'data/job_tuples.pickle'
+
+
+def load_jobs(jq):
+    now = time()
+
+    with open(JOBS_PICKLE, 'rb') as fp:
+        while True:
+            try:
+                next_t, job = pickle.load(fp)
+            except EOFError:
+                break  # Loaded all job tuples
+
+            # Create threading primitives
+            enabled = job._enabled
+            removed = job._remove
+
+            job._enabled = Event()
+            job._remove = Event()
+
+            if enabled:
+                job._enabled.set()
+
+            if removed:
+                job._remove.set()
+
+            next_t -= now  # Convert from absolute to relative time
+
+            jq._put(job, next_t)
+
+
+def save_jobs(jq):
+    if jq:
+        job_tuples = jq._queue.queue
+    else:
+        job_tuples = []
+
+    with open(JOBS_PICKLE, 'wb') as fp:
+        for next_t, job in job_tuples:
+            # Back up objects
+            _job_queue = job._job_queue
+            _remove = job._remove
+            _enabled = job._enabled
+
+            # Replace un-pickleable threading primitives
+            job._job_queue = None  # Will be reset in jq.put
+            job._remove = job.removed  # Convert to boolean
+            job._enabled = job.enabled  # Convert to boolean
+
+            # Pickle the job
+            pickle.dump((next_t, job), fp)
+
+            # Restore objects
+            job._job_queue = _job_queue
+            job._remove = _remove
+            job._enabled = _enabled
+
+
+def save_jobs_job(bot, job):
+    save_jobs(job.job_queue)
+
+
 def launch_bot():
     updater = Updater(settings.TELEGRAM_BOT_TOKEN)
+
+    job_queue = updater.job_queue
+
+    job_queue.run_repeating(save_jobs_job, timedelta(minutes=1))
+
+    try:
+        load_jobs(job_queue)
+
+    except FileNotFoundError:
+        # First run
+        pass
 
     conv_handler = ConversationHandler(
         entry_points=[
@@ -368,6 +443,9 @@ def launch_bot():
 
     updater.start_polling()
     updater.idle()
+
+    save_jobs(job_queue)
+
     logger.info("Started bot")
 
 
