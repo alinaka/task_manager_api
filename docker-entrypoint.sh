f0a517ee7369ccc4fba397ad33db8eba9d0d7b02
Alina Gotovtseva <alinakago@gmail.com>
2019-08-26 13:42:06 +0300

    edited gunicorn command

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-gunicorn --workers=1 --threads=2 --bind 0.0.0.0:8000 task_manager_api.wsgi:application -p /code/gunicorn.pid --log-level=info --access-logfile=/var/log/task_manager_api/access.log --error-logfile=/var/log/task_manager_api/error.log --timeout 300 --capture-output
+gunicorn task_manager_api.wsgi:application -p /code/gunicorn.pid --timeout 300 --capture-output
